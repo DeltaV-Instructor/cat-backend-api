0f154eeb632b1f78d9907fa9c955ee9fd35ee0a6
tektechnologies <info@tektechnologies.com>
2023-05-02 11:42:29 -0500

    add post and delete

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+
+// USE
+// implement express .get(), .use(), .post(), delete()
 const app = express();
 app.use(cors());
+app.use(express.json());
+
 
 mongoose.connect(
   process.env.DB_URL
@@ -17,12 +22,16 @@ mongoose.connect(
 const Cat = require('./models/cat.js');
 const PORT = process.env.PORT || 5005;
 
-
+// app.whateverMethod('/endPoint', callback())
 app.get('/', (req,res) =>res.status(200).send('Hello from our Server!'));
 app.get('/cats', getCats);
+app.post('/cats', postCats);
+app.delete('/cats/:id', deleteCats);
+
 
 
 async function getCats(req,res){
+  console.log('called.');
   try {
     let results = await Cat.find();
     console.log('results',results);
@@ -31,11 +40,39 @@ async function getCats(req,res){
     next(error);
   }
 }
+// LAB 12 - FEATURED TASKS
+// Add a new route and handler function to your server, to respond to `POST` requests to `/books`. This is your book-creation end point. Verify it's working by sending a raw POST request via your REST Client.
+// Be sure to include server-side error checking, in case something goes wrong. Respond with appropriate status codes, if anything goes wrong. Verify with your REST Client.
+// Add a server end point to handle `DELETE` requests to `/books/:id`.
+
+// {
+//   "name": "BOB",
+//   "color":"Blue",
+//   "spayNeuter": true,
+//   "location": "the streets"
+// }
+async function postCats(request, response, next){
+  console.log(request.body);
+  try {
+    let createCat = await Cat.create(request.body);
+    response.status(200).send(createCat);
+  } catch (error) {
+    next(error);
+  }
+}
 
 
-
-
-
+async function deleteCats(req, res, next){
+  console.log(req.params.id);
+  try {
+    //Model.findByIdAndDelete()
+    let id = req.params.id;
+    await Cat.findByIdAndDelete(id);
+    res.status(200).send('Cat was deleted.');
+  } catch (error) {
+    next(error);
+  }
+}
 
 
 //star do?
